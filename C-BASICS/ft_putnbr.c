997a75584d81d056ab122ab6dadc59ba36d9ce21
Matias <Matias@matias.home>
2026-07-12 19:14:31 +0200

    training fait maison avec amour

--- C-BASICS/ft_putnbr.c
+++ C-BASICS/ft_putnbr.c
@@ -7,34 +7,34 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	int modulo;
-	int divid;
+	int	mod;
+	int	div;
 
-	if (nb == -2147483648)
+	if(nb == -2147483648)
 	{
 		write(1, "-2147483648", 11);
 		return ;
 	}
-	if (nb < 0)
+	if(nb < 0)
 	{
 		ft_putchar('-');
 		nb = -nb;
 	}
-	if (nb > 9)
+	if(nb > 9)
 	{
-		modulo = nb % 10;
-		divid = nb / 10;
-		ft_putnbr(divid);
+		mod = nb % 10;
+		div = nb / 10;
+		ft_putnbr(div);
 	}
-	if (nb > 9)
-		ft_putchar(modulo + '0');
+	if(nb > 9)
+		ft_putchar(mod + '0');
 	else
 		ft_putchar(nb + '0');
 }
 
 int	main(void)
 {
-	ft_putnbr(9);
-	ft_putnbr(-2147483648);
-	ft_putnbr(-390);
+	ft_putnbr(-345678);
+	ft_putnbr(1);
+	ft_putnbr(-123);
 }
